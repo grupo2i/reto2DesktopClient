563e0f84f8422d67364e95013a9777d77664bb2c
grupo2i <72964155+grupo2i@users.noreply.github.com>
2021-01-26 14:34:35 +0100

    Update ArtistRESTClient.java

--- src/reto2desktopclient/client/ArtistRESTClient.java
+++ src/reto2desktopclient/client/ArtistRESTClient.java
@@ -4,6 +4,7 @@ import java.util.ResourceBundle;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 
 /**
  * Jersey REST client generated for REST resource:ArtistFacadeREST
@@ -24,11 +25,43 @@ public class ArtistRESTClient implements ArtistManager {
     private Client client;
     //TODO: Leer la URL de un archivo de propiedades
     private static final String BASE_URI = ResourceBundle
-            .getBundle("reto2desktopclient.properties.properties").getString("BASE_URI");;
+            .getBundle("reto2desktopclient.properties.properties").getString("BASE_URI");
+
+    ;
 
     public ArtistRESTClient() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
         webTarget = client.target(BASE_URI).path("entity.artist");
+    }
+
+    /**
+     *
+     * @param requestEntity
+     * @throws ClientErrorException
+     */
+    @Override
+    public void edit(Object requestEntity) throws ClientErrorException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
+    }
+
+    /**
+     *
+     * @param <T>
+     * @param responseType
+     * @param id
+     * @return
+     * @throws ClientErrorException
+     */
+    @Override
+    public <T> T find(Class<T> responseType, String id) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+
+    @Override
+    public void create(Object requestEntity) throws ClientErrorException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
 
     public <T> T getAllArtists(Class<T> responseType) throws ClientErrorException {
@@ -37,26 +70,22 @@ public class ArtistRESTClient implements ArtistManager {
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
 
-    public void edit(Object requestEntity) throws ClientErrorException {
-        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
-    }
-
-    public <T> T find(Class<T> responseType, String id) throws ClientErrorException {
-        WebTarget resource = webTarget;
-        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
-        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
-    }
-
-    public void create(Object requestEntity) throws ClientErrorException {
-        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
-    }
-
+    @Override
     public void remove(String id) throws ClientErrorException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request().delete();
     }
 
+    @Override
     public void close() {
         client.close();
     }
+
     
+    @Override
+    public <T> T getAllArtists(GenericType<T> responseType) throws ClientErrorException {
+      WebTarget resource = webTarget;
+        resource = resource.path("getAllArtists");
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+      }
+
 }
